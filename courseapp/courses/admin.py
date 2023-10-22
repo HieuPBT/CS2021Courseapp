d5e39ad3bd826ad28cdb5633e29f883cd2c1ae75
Hieupbt <hieuphambatrung@gmail.com>
2023-10-22 15:21:23 +0700

    update

---
+++ courseapp/courses/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Category, Course
+# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'name']
+    list_filter = ['id', 'name']
+    search_fields = ['id', 'name']
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Course)
